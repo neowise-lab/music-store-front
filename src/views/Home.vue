3e6dd9e5615c51d64df7d4e8bd41dc38c6180493
neowise <jalolimomaddinov97@gmail.com>
2020-10-08 04:22:31 +0300

    First Vue.js Todo app

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut aliquid voluptate consectetur? Iusto modi nam ea. Quod debitis, dolor quo obcaecati blanditiis maxime nihil sint laborum nesciunt quisquam expedita facilis.</p>
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
---
+++ src/views/Todos.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h2>Todo application</h2>
+    <router-link to="/">Home</router-link>
+    <AddTodo
+      @add-todo="addTodo"
+    />
+    <hr>
+    <Loader v-if="loading"/>
+    <TodoList
+      v-else-if="todos.length"
+      v-bind:todos="todos"
+      @remove-todo="removeTodo"
+    />
+    <p v-else>No Todos!</p>
+  </div>
+</template>
+
+<script>
+
+import TodoList from "@/components/TodoList"
+import AddTodo from "@/components/AddTodo"
+import Loader from "@/components/Loader"
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      todos: [],
+      loading: true
+    }
+  },
+  components: {
+    TodoList, 
+    AddTodo, 
+    Loader
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
+      .then(response => response.json())
+      .then(json => {
+          setTimeout(() => {
+                this.todos = json
+                this.loading = false
+          }, 1000)
+      })
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id)
+    },
+    addTodo(todo) {
+      this.todos.push(todo)
+    }
+  }
+}
+</script>
